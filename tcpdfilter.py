f796692d6a1dc33255a418f29b78ad3e0461f8fa
Eric Gazoni <eric@adimian.com>
2018-07-02 19:50:01 +0200

    add sport, dport to rules

--- tcpdfilter.py
+++ tcpdfilter.py
@@ -2,6 +2,7 @@ import yaml
 import attr
 from argparse import ArgumentParser
 from scapy.all import rdpcap
+from prettytable import PrettyTable
 
 import colorama
 from colorama import Fore, Back, Style
@@ -20,9 +21,9 @@ class PacketSummary:
     matching_rule = attr.ib()
 
     def __str__(self):
-        text = 'src: {}, dst: {}, sport: {}, dport: {}'.format(self.src, 
-                                                               self.dst, 
-                                                               self.sport, 
+        text = 'src: {}, dst: {}, sport: {}, dport: {}'.format(self.src,
+                                                               self.dst,
+                                                               self.sport,
                                                                self.dport)
         if self.matching_rule:
             text = text + ' rule: {}'.format(self.matching_rule.name)
@@ -33,25 +34,32 @@ class WhitelistRule:
     name = attr.ib()
     src = attr.ib(default='*')
     dst = attr.ib(default='*')
-    port = attr.ib(default='*')
+    sport = attr.ib(default='*')
+    dport = attr.ib(default='*')
+    triggered = attr.ib(default=False)
 
     rule_to_packet_mapping = {
-        'port': 'dport'
     }
+
+    def __str__(self):
+        return '{} > src:{} dst:{} sport:{} dport:{}'.format(
+            self.name, self.src, self.dst, self.sport, self.dport
+        )
 
     def matches(self, packet):
         if not 'IP' in packet:
             raise ValueError('this is not an IP packet')
 
         ip_layer = packet['IP']
-        for attribute in ('src', 'dst', 'port'):
+        for attribute in ('src', 'dst', 'sport', 'dport'):
             rule_value = getattr(self, attribute)
             if rule_value != '*':
-                packet_attribute = self.rule_to_packet_mapping.get(attribute, 
+                packet_attribute = self.rule_to_packet_mapping.get(attribute,
                                                                    attribute)
                 packet_value = getattr(ip_layer, packet_attribute)
                 if packet_value != rule_value:
                     return False
+        self.triggered = True
         return True
 
 def parse_rule(definition):
@@ -89,14 +97,13 @@ def filter_packets(packets, rules):
 
         for rule in rules:
             if rule.matches(packet):
-                output['whitelisted'].add(packet_summary(packet, 
+                output['whitelisted'].add(packet_summary(packet,
                                                          matching_rule=rule))
                 break
         else:
             output['blacklisted'].add(packet_summary(packet))
 
     return output
-
 
 def main(whitelist_path, packet_capture_path):
     rules = read_whitelist(open(whitelist_path).read())
@@ -108,15 +115,33 @@ def main(whitelist_path, packet_capture_path):
     filtered_packets = filter_packets(packets, rules)
 
     print('Whitelisted:')
+    table = PrettyTable(field_names=('src', 'dst', 'sport', 'dport', 'rule'))
+    for col in table.align:
+        table.align[col] = "r"
+    table.align['rule'] = "l"
+    for row in filtered_packets['whitelisted']:
+        table.add_row(
+            (row.src, row.dst, row.sport, row.dport, row.matching_rule.name)
+        )
+    print(Fore.GREEN + str(table))
 
-    for packet in filtered_packets['whitelisted']:
-        print(Fore.GREEN + str(packet))
     print(Style.RESET_ALL)
-
     print('Blacklisted:')
 
-    for packet in filtered_packets['blacklisted']:
-        print(Fore.RED + str(packet))
+    table = PrettyTable(field_names=('src', 'dst', 'sport', 'dport'))
+    for col in table.align:
+        table.align[col] = "r"
+    for row in filtered_packets['blacklisted']:
+        table.add_row(
+            (row.src, row.dst, row.sport, row.dport)
+        )
+    print(Fore.RED + str(table))
+    print(Style.RESET_ALL)
+
+    print('Rules not triggered:')
+    for rule in rules:
+        if not rule.triggered:
+            print(Fore.YELLOW + str(rule))
     print(Style.RESET_ALL)
 
 
